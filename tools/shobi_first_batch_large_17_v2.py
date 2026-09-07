3d6b483212e4040f3f967239f33e77ae258b69b9
MarvvRed <eronius85@gmail.com>
2026-09-07 15:48:07 +0200

    Add Shobi-first batch 17

---
+++ tools/shobi_first_batch_large_17_v2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+APPROVED_BATCH_LARGE_17 = {
+    '387-': (9351, 'Remy Latour', 'Cigar', 'Shobi residual is CIGAR; Fragrantica exact Remy Latour Cigar 1996 entry ID 9351'),
+}
