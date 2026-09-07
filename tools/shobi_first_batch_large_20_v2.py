10a2df2736f77f3069c92c8601facbf7b73dab86
MarvvRed <eronius85@gmail.com>
2026-09-07 16:14:16 +0200

    Add Shobi-first batch 20

---
+++ tools/shobi_first_batch_large_20_v2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+APPROVED_BATCH_LARGE_20 = {
+    '2098-MIC': ('50396', 'Michael Kors', 'Twilight Shimmer', 'Shobi residual is Twilight Shimmer under Michael Kors code; exact Fragrantica entry is Twilight Shimmer 2018'),
+    '2773-RIT': ('83081', 'Rituals', 'The Rituals of Mehr Body Mist', 'Shobi residual is The Rituals of Mehr; exact Fragrantica entry is The Rituals of Mehr Body Mist 2023'),
+}
